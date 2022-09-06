701a7806fc5f7a91e127dc1639411746c7980bef
mertcuan <85804481+mertcuan@users.noreply.github.com>
2022-09-06 22:06:16 +0300

    Add files via upload

---
+++ for/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+
+
+            for (int i = 1; i <= 999999999; i++)
+            {
+                for (int x = 1; x < i; x++)
+                {
+                    Console.Write(x);
+                }
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
